158eb262613e2bb5b0ae0d760975d682cfbc2d4c
Luca Monari <we297@uni-heidelberg.de>
2024-06-25 11:23:26 +0200

    updated the handler of the web_app

--- st_oxview/__init__.py
+++ st_oxview/__init__.py
@@ -2,35 +2,28 @@ import streamlit.components.v1 as components
 import os
 
 parent_dir = os.path.dirname(os.path.abspath(__file__))
-oxview_static_folder = os.path.join(os.path.join(parent_dir, "oxview_src"), "static")
+oxview_static_folder = os.path.join(parent_dir, "oxview_src")
 
 _component_func = components.declare_component(
     "oxview_frame",
     path=parent_dir,
 )
 
-def oxview_from_text(configuration=None, topology=None, forces=None, pdb=None, width='99%', height=500, **kwargs):
-    texts = [configuration, topology, forces, pdb]
-    names = ["struct.dat", "struct.top", "structforces.txt", "struct.pdb"]
-    oxview_file_type = ["configuration", "topology", "forces", "file"]
-    oxdna_file_text = ''
-    for text, name, ox_name in zip(texts, names, oxview_file_type):
-        if text is not None:
-            with open(os.path.join(oxview_static_folder, name), "wb") as f:
-                f.write(text)
-            oxdna_file_text += f'&{ox_name}=.%2Fstatic%2F{name}'
-    _component_func(files_text = oxdna_file_text[1:], width=width, height=height, **kwargs)
-
 def oxview_from_file(configuration=None, topology=None, forces=None, pdb=None, width='99%', height=500, **kwargs):
+    """Create an OxView component loading the data from the specified files and displaying it in a frame.
+    The files must be in the static folder of the app."""
     srcs = [configuration, topology, forces, pdb]
-    names = ["struct.dat", "struct.top", "structforces.txt", "struct.pdb"]
-    oxview_file_type = ["configuration", "topology", "forces", "file"]
-    oxdna_file_text = ''
-    for src, name, ox_name in zip(srcs, names, oxview_file_type):
-        if src is not None:
-            with open(src, "rb") as f:
-                src_content = f.read()
-            with open(os.path.join(oxview_static_folder, name), "wb") as f:
-                f.write(src_content)
-            oxdna_file_text += f'&{ox_name}=.%2Fstatic%2F{name}'
-    _component_func(files_text = oxdna_file_text[1:], width=width, height=height, **kwargs)
+    oxview_extension = ["dat", "top", "txt", "pdb"]
+    for i in range(len(srcs)-1, -1, -1):
+        if not srcs[i]:
+            srcs.pop(i)
+            oxview_extension.pop(i)
+        else:
+            # Construct the URL to the static file
+            srcs[i] = os.path.sep + 'app' + os.path.sep + 'static' + os.path.sep + os.path.basename(srcs[i])
+    if not srcs:
+        srcs = ['']
+        oxview_extension = ['']
+    
+    component_value = _component_func(files_path=srcs, files_extension=oxview_extension, width=width, height=height, **kwargs)
+    return component_value
