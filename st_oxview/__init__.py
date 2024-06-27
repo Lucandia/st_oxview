79c28f67a321e3884a32f0a16a51ef549856073b
Luca Monari <we297@uni-heidelberg.de>
2024-06-27 18:21:46 +0200

    added try and catch error message in oxview functions

--- st_oxview/__init__.py
+++ st_oxview/__init__.py
@@ -16,9 +16,12 @@ def oxview_from_text(configuration=None, topology=None, forces=None, pdb=None, w
     oxdna_file_text = ''
     for text, name, ox_name in zip(texts, names, oxview_file_type):
         if text is not None:
-            with open(os.path.join(oxview_static_folder, name), "w") as f:
-                f.write(text)
-            oxdna_file_text += f'&{ox_name}=.%2Fstatic%2F{name}'
+            try:
+                with open(os.path.join(oxview_static_folder, name), "w") as f:
+                    f.write(text)
+                oxdna_file_text += f'&{ox_name}=.%2Fstatic%2F{name}'
+            except Exception as e:
+                print(e)
     _component_func(files_text = oxdna_file_text[1:], width=width, height=height, **kwargs)
 
 def oxview_from_file(configuration=None, topology=None, forces=None, pdb=None, width='99%', height=500, **kwargs):
@@ -28,9 +31,12 @@ def oxview_from_file(configuration=None, topology=None, forces=None, pdb=None, w
     oxdna_file_text = ''
     for src, name, ox_name in zip(srcs, names, oxview_file_type):
         if src is not None:
-            with open(src, "r") as f:
-                src_content = f.read()
-            with open(os.path.join(oxview_static_folder, name), "w") as f:
-                f.write(src_content)
-            oxdna_file_text += f'&{ox_name}=.%2Fstatic%2F{name}'
+            try:
+                with open(src, "r") as f:
+                    src_content = f.read()
+                with open(os.path.join(oxview_static_folder, name), "w") as f:
+                    f.write(src_content)
+                oxdna_file_text += f'&{ox_name}=.%2Fstatic%2F{name}'
+            except Exception as e:
+                print(e)
     _component_func(files_text = oxdna_file_text[1:], width=width, height=height, **kwargs)
