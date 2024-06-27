74efed45a5d97ea17a7d9f4e05cd9664a458c1ad
Luca Monari <we297@uni-heidelberg.de>
2024-06-27 18:20:34 +0200

    use write in stead of wb

--- st_oxview/__init__.py
+++ st_oxview/__init__.py
@@ -16,7 +16,7 @@ def oxview_from_text(configuration=None, topology=None, forces=None, pdb=None, w
     oxdna_file_text = ''
     for text, name, ox_name in zip(texts, names, oxview_file_type):
         if text is not None:
-            with open(os.path.join(oxview_static_folder, name), "wb") as f:
+            with open(os.path.join(oxview_static_folder, name), "w") as f:
                 f.write(text)
             oxdna_file_text += f'&{ox_name}=.%2Fstatic%2F{name}'
     _component_func(files_text = oxdna_file_text[1:], width=width, height=height, **kwargs)
@@ -28,9 +28,9 @@ def oxview_from_file(configuration=None, topology=None, forces=None, pdb=None, w
     oxdna_file_text = ''
     for src, name, ox_name in zip(srcs, names, oxview_file_type):
         if src is not None:
-            with open(src, "rb") as f:
+            with open(src, "r") as f:
                 src_content = f.read()
-            with open(os.path.join(oxview_static_folder, name), "wb") as f:
+            with open(os.path.join(oxview_static_folder, name), "w") as f:
                 f.write(src_content)
             oxdna_file_text += f'&{ox_name}=.%2Fstatic%2F{name}'
     _component_func(files_text = oxdna_file_text[1:], width=width, height=height, **kwargs)
